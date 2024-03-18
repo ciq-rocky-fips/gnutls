126ba9bfdebe31df7c503d238ef0795ffdf2b15e
Jeremy Allison <jallison@ciq.com>
2024-03-18 10:25:19 -0700

    Add untarred pristine gnutls-3.7.6 source code directory.
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

---
+++ cligen/cligen.mk
@@ -0,0 +1,6 @@
+cligen_sources =						\
+	cligen/cli-codegen.py cligen/cli-docgen.py		\
+	cligen/cligen/__init__.py cligen/cligen/code.py		\
+	cligen/cligen/package.py cligen/cligen/types.py		\
+	cligen/cligen/doc/__init__.py cligen/cligen/doc/man.py	\
+	cligen/cligen/doc/texi.py
