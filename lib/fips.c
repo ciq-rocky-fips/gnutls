ad15aea222ba0de5d74ab835e91ce23f92ad8064
Jeremy Allison <jallison@ciq.com>
2024-08-26 14:05:46 -0700

    SUSE: Patch18: gnutls-FIPS-Zeroize-check_binary_integrity.patch
    Modified from patchfile.
    
    Change gnutls_memset(hmac, 0, HMAC_SIZE) to zeroize_key(hmac, sizeof(hmac))
    
    Currently they are the same function but this makes things clearer.

--- lib/fips.c
+++ lib/fips.c
@@ -358,12 +358,12 @@ static int check_lib_hmac(struct hmac_entry *entry, const char *path)
 
 	if (gnutls_memcmp(entry->hmac, hmac, HMAC_SIZE)) {
 		_gnutls_debug_log("Calculated MAC for %s does not match\n", path);
-		gnutls_memset(hmac, 0, HMAC_SIZE);
+		zeroize_key(hmac, sizeof(hmac));
 		return gnutls_assert_val(GNUTLS_E_PARSING_ERROR);
 	}
 	_gnutls_debug_log("Successfully verified MAC for %s\n", path);
 
-	gnutls_memset(hmac, 0, HMAC_SIZE);
+	zeroize_key(hmac, sizeof(hmac));
 	return 0;
 }
 
