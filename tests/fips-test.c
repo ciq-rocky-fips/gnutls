550734a3fe54892f709c8e174c9e4d41a6a9a240
Jeremy Allison <jallison@ciq.com>
2025-05-21 17:02:39 -0700

    Fix the PBKDF and gnutls_privkey_XXX() tests to work with the new restrictions.
    These probably should be folded into the relevent SUSE patches that
    added the restrictions, but the SUSE fips-test.c code doesn't have
    these tests so they didn't add these changes with the restrictions.
    
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

--- tests/fips-test.c
+++ tests/fips-test.c
@@ -22,6 +22,7 @@ static void tls_log_func(int level, const char *str)
 
 static uint8_t key13[13];
 static uint8_t key16[16];
+static uint8_t key20[20];
 static uint8_t iv16[16];
 static uint8_t key_data[64];
 static uint8_t iv_data[16];
@@ -341,6 +342,7 @@ void doit(void)
 	gnutls_privkey_t privkey;
 	gnutls_datum_t key_invalid = { key13, sizeof(key13) };
 	gnutls_datum_t key = { key16, sizeof(key16) };
+	gnutls_datum_t pbkdfkey = { key20, sizeof(key20) };
 	gnutls_datum_t iv = { iv16, sizeof(iv16) };
 	gnutls_datum_t signature;
 	unsigned int bits;
@@ -454,29 +456,29 @@ void doit(void)
 	}
 	FIPS_POP_CONTEXT(NOT_APPROVED);
 
-	/* PBKDF2 with key equal to or longer than 112 bits: approved */
+	/* PBKDF2 with key equal to or longer than 160 bits: approved */
 	FIPS_PUSH_CONTEXT();
-	ret = gnutls_pbkdf2(GNUTLS_MAC_SHA256, &key, &iv, 1000, &pbkdf2,
+	ret = gnutls_pbkdf2(GNUTLS_MAC_SHA256, &pbkdfkey, &iv, 1000, &pbkdf2,
 			    sizeof(pbkdf2));
 	if (ret < 0) {
 		fail("gnutls_pbkdf2 failed\n");
 	}
 	FIPS_POP_CONTEXT(APPROVED);
 
-	/* PBKDF2 with key shorter than 112 bits: not approved */
+	/* PBKDF2 with key shorter than 160 bits: not approved */
 	FIPS_PUSH_CONTEXT();
-	key.size = 13;
-	ret = gnutls_pbkdf2(GNUTLS_MAC_SHA256, &key, &iv, 1000, &pbkdf2,
+	pbkdfkey.size = 16;
+	ret = gnutls_pbkdf2(GNUTLS_MAC_SHA256, &pbkdfkey, &iv, 1000, &pbkdf2,
 			    sizeof(pbkdf2));
 	if (ret < 0) {
 		fail("gnutls_pbkdf2 failed\n");
 	}
-	key.size = sizeof(key16);
+	pbkdfkey.size = sizeof(key20);
 	FIPS_POP_CONTEXT(NOT_APPROVED);
 
 	/* PBKDF2 with iteration count lower than 1000: not approved */
 	FIPS_PUSH_CONTEXT();
-	ret = gnutls_pbkdf2(GNUTLS_MAC_SHA256, &key, &iv, 999, &pbkdf2,
+	ret = gnutls_pbkdf2(GNUTLS_MAC_SHA256, &pbkdfkey, &iv, 999, &pbkdf2,
 			    sizeof(pbkdf2));
 	if (ret < 0) {
 		fail("gnutls_pbkdf2 failed\n");
@@ -486,7 +488,7 @@ void doit(void)
 	/* PBKDF2 with salt shorter than 16 bytes: not approved */
 	FIPS_PUSH_CONTEXT();
 	iv.size = 13;
-	ret = gnutls_pbkdf2(GNUTLS_MAC_SHA256, &key, &iv, 1000, &pbkdf2,
+	ret = gnutls_pbkdf2(GNUTLS_MAC_SHA256, &pbkdfkey, &iv, 1000, &pbkdf2,
 			    sizeof(pbkdf2));
 	if (ret < 0) {
 		fail("gnutls_pbkdf2 failed\n");
@@ -496,7 +498,7 @@ void doit(void)
 
 	/* PBKDF2 with output shorter than 112 bits: not approved */
 	FIPS_PUSH_CONTEXT();
-	ret = gnutls_pbkdf2(GNUTLS_MAC_SHA256, &key, &iv, 1000, &pbkdf2, 13);
+	ret = gnutls_pbkdf2(GNUTLS_MAC_SHA256, &pbkdfkey, &iv, 1000, &pbkdf2, 13);
 	if (ret < 0) {
 		fail("gnutls_pbkdf2 failed\n");
 	}
@@ -716,7 +718,7 @@ void doit(void)
 	if (ret < 0) {
 		fail("gnutls_privkey_sign_data2 failed\n");
 	}
-	FIPS_POP_CONTEXT(NOT_APPROVED);
+	FIPS_POP_CONTEXT(ERROR);
 
 	/* Verify a signature created with ECDSA and SHA-1; not approved */
 	FIPS_PUSH_CONTEXT();
@@ -733,10 +735,10 @@ void doit(void)
 	FIPS_PUSH_CONTEXT();
 	ret = gnutls_privkey_sign_data(privkey, GNUTLS_DIG_SHA1, 0, &data,
 				       &signature);
-	if (ret < 0) {
-		fail("gnutls_privkey_sign_data failed\n");
+	if (ret == 0) {
+		fail("gnutls_privkey_sign_data succeeded - should fail\n");
 	}
-	FIPS_POP_CONTEXT(NOT_APPROVED);
+	FIPS_POP_CONTEXT(ERROR);
 	gnutls_free(signature.data);
 
 	/* Create a SHA1 hashed data for 2-pass signature API; not a
