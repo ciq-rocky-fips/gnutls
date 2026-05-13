69ad62d569626ebcee00c75d8852a557274ac690
Jeremy Allison <jallison@ciq.com>
2026-05-13 12:37:00 -0700

    gnutls_cipher_decrypt3: make PKCS#7 unpadding branch free
    Back-ported from upstream commit: 1e627aa5ad95c6dc0518d94e9a009997b081a1ab
    
    No new test added.
    
    	Author: Daiki Ueno <ueno@gnu.org>
    	Date:   Wed Apr 1 18:57:21 2026 +0900
    
    	gnutls_cipher_decrypt3: make PKCS#7 unpadding branch free
    
    	This tries to make the logic of PKCS#7 padding removal constant-time,
    	by removing potential branching operations.
    
    	Reported-by: Doria Tang of Stony Brook University
    	Fixes: #1815
    	Fixes: CVE-2026-5419
    	Fixes: GNUTLS-SA-2026-04-29-13
    	CVSS: 3.7 Low CVSS:3.1/AV:N/AC:H/PR:N/UI:N/S:U/C:L/I:N/A:N
    	Signed-off-by: Daiki Ueno <ueno@gnu.org>
    
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

--- lib/crypto-api.c
+++ lib/crypto-api.c
@@ -519,6 +519,39 @@ gnutls_cipher_encrypt3(gnutls_cipher_hd_t handle,
 	return ret;
 }
 
+/* If succeeds, returns the number of padding bytes to be removed;
+ * zero otherwise.
+ */
+unsigned int _gnutls_pkcs7_unpad(const uint8_t *block, unsigned int block_size)
+{
+	uint8_t padding = block[block_size - 1];
+	volatile unsigned int mask = ~0;
+	volatile unsigned int count = 0;
+
+	/* Count consecutive PADDING bytes from the end, in a
+	 * constant-time manner.
+	 */
+	for (size_t i = block_size; i > 0; i--) {
+		volatile unsigned int mask2;
+
+		mask2 = -(unsigned int)(block[i - 1] == padding);
+		mask2 &= -(unsigned int)(count < padding);
+
+		/* MASK is initially ~0 and will be flipped to 0 upon first
+		 * non-padding bytes.
+		 */
+		mask &= mask2;
+		count += 1 & mask;
+	}
+
+	/* PADDING == 0 is effectively excluded here, given COUNT
+	 * will never be 0.
+	 */
+	mask = -(unsigned int)(count <= block_size);
+	mask &= -(unsigned int)(count == padding);
+	return count & mask;
+}
+
 /**
  * gnutls_cipher_decrypt3:
  * @handle: is a #gnutls_cipher_hd_t type
